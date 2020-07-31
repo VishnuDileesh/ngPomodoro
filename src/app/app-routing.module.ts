25a64ac51caa2e655d53297315c34487fb234dbe
Vishnu Dileesh <dev@vishnudileesh.com>
2020-07-31 11:28:30 +0530

    Set up focus component route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FocusComponent } from './pages/focus/focus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/focus', pathMatch: 'full' },
+  { path: 'focus', component: FocusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FocusComponent } from './pages/focus/focus.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    FocusComponent
   ],
   imports: [
     BrowserModule,
