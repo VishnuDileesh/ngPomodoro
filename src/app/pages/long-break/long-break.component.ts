bf1df2d14d3fb752c38641cb794b4175708bea1b
Vishnu Dileesh <dev@vishnudileesh.com>
2020-07-31 12:57:26 +0530

    Add - long break timer functionality

--- src/app/pages/long-break/long-break.component.ts
+++ src/app/pages/long-break/long-break.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerconfigService } from 'src/app/timerconfig.service';
 
 @Component({
   selector: 'app-long-break',
   templateUrl: './long-break.component.html',
-  styleUrls: ['./long-break.component.css']
+  styleUrls: ['./long-break.component.css'],
 })
 export class LongBreakComponent implements OnInit {
+  time;
+  timeConfig: any;
 
-  constructor() { }
+  constructor(private timerConfigService: TimerconfigService) {}
 
   ngOnInit(): void {
+    this.time = this.timerConfigService.getLongBreakTime();
+    this.timeConfig = {
+      leftTime: this.time * 60,
+    };
   }
-
 }
