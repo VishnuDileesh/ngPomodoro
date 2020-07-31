f71a917613bddf1470f5cdc8322ccb93a9221538
Vishnu Dileesh <dev@vishnudileesh.com>
2020-07-31 12:54:40 +0530

    Add - short break timer functionality

--- src/app/pages/short-break/short-break.component.ts
+++ src/app/pages/short-break/short-break.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TimerconfigService } from 'src/app/timerconfig.service';
 
 @Component({
   selector: 'app-short-break',
   templateUrl: './short-break.component.html',
-  styleUrls: ['./short-break.component.css']
+  styleUrls: ['./short-break.component.css'],
 })
 export class ShortBreakComponent implements OnInit {
+  time;
+  timeConfig: any;
 
-  constructor() { }
+  constructor(private timerConfigService: TimerconfigService) {}
 
   ngOnInit(): void {
+    this.time = this.timerConfigService.getShortBreakTime();
+    this.timeConfig = {
+      leftTime: this.time * 60,
+    };
   }
-
 }
